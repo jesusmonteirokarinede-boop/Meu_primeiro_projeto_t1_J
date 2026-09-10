392d38175c5127d7ffe32912abbd8bf0f189c71d
48239601812 <jesusmonteirokarinede@gmail.com>
2026-09-10 14:37:06 -0300

    exercicio 4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,22 +1,36 @@
-import java.util.Locale;
 import java.util.Scanner;
 
-class questao3 {
+class Questao4 {
     public static void main(String[] args) {
+        try (Scanner ent = new Scanner(System.in)) {
 
-        try (Scanner ent = new Scanner(System.in).useLocale(Locale.US)) {
+            System.out.print("Informe o dia da semana (1 a 7): ");
+            int dia = ent.nextInt();
 
-            System.out.print("Informe a sua nota: ");
-            double nota = ent.nextDouble();
-
-            if (nota >= 9) {
-                System.out.println("Muito Bom!");
-            } else if (nota >= 7) {
-                System.out.println("Bom");
-            } else if (nota >= 5) {
-                System.out.println("Regular");
-            } else {
-                System.out.println("Irregular!");
+            switch (dia) {
+                case 1:
+                    System.out.println("Segunda-feira");
+                    break;
+                case 2:
+                    System.out.println("Terça-feira");
+                    break;
+                case 3:
+                    System.out.println("Quarta-feira");
+                    break;
+                case 4:
+                    System.out.println("Quinta-feira");
+                    break;
+                case 5:
+                    System.out.println("Sexta-feira");
+                    break;
+                case 6:
+                    System.out.println("Sábado");
+                    break;
+                case 7:
+                    System.out.println("Domingo");
+                    break;
+                default:
+                    System.out.println("Dia inválido");
             }
         }
     }
