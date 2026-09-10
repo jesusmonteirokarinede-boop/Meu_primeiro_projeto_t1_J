31e091d89aedf5779282fcb713fd1715b2b43619
48239601812 <jesusmonteirokarinede@gmail.com>
2026-09-10 14:23:56 -0300

    exercicio 3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,22 @@
 import java.util.Locale;
 import java.util.Scanner;
 
-
-class Questao2 {
+class questao3 {
     public static void main(String[] args) {
+
         try (Scanner ent = new Scanner(System.in).useLocale(Locale.US)) {
 
             System.out.print("Informe a sua nota: ");
             double nota = ent.nextDouble();
 
-            if (nota >= 7) {
-                System.out.println("Aprovado");
+            if (nota >= 9) {
+                System.out.println("Muito Bom!");
+            } else if (nota >= 7) {
+                System.out.println("Bom");
+            } else if (nota >= 5) {
+                System.out.println("Regular");
             } else {
-                System.out.println("Reprovado");
+                System.out.println("Irregular!");
             }
         }
     }
