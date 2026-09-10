c31b64d6aa314c8ce2a4067a0a8a0f99682dcb5e
48239601812 <jesusmonteirokarinede@gmail.com>
2026-09-10 14:41:14 -0300

    exercicio 4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,36 +2,37 @@ import java.util.Scanner;
 
 class Questao4 {
     public static void main(String[] args) {
-        try (Scanner ent = new Scanner(System.in)) {
+        Scanner ent = new Scanner(System.in);
 
-            System.out.print("Informe o dia da semana (1 a 7): ");
-            int dia = ent.nextInt();
+        System.out.print("Informe o dia da semana (1 a 7): ");
+        int dia = ent.nextInt();
 
-            switch (dia) {
-                case 1:
-                    System.out.println("Segunda-feira");
-                    break;
-                case 2:
-                    System.out.println("Terça-feira");
-                    break;
-                case 3:
-                    System.out.println("Quarta-feira");
-                    break;
-                case 4:
-                    System.out.println("Quinta-feira");
-                    break;
-                case 5:
-                    System.out.println("Sexta-feira");
-                    break;
-                case 6:
-                    System.out.println("Sábado");
-                    break;
-                case 7:
-                    System.out.println("Domingo");
-                    break;
-                default:
-                    System.out.println("Dia inválido");
-            }
+        switch (dia) {
+            case 1:
+                System.out.println("Segunda-feira");
+                break;
+            case 2:
+                System.out.println("Terça-feira");
+                break;
+            case 3:
+                System.out.println("Quarta-feira");
+                break;
+            case 4:
+                System.out.println("Quinta-feira");
+                break;
+            case 5:
+                System.out.println("Sexta-feira");
+                break;
+            case 6:
+                System.out.println("Sábado");
+                break;
+            case 7:
+                System.out.println("Domingo");
+                break;
+            default:
+                System.out.println("Inexistente");
         }
+
+        ent.close();
     }
 }
