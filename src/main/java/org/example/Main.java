61e4b99e2fe4ddb6238d47c0220b2c4334d15789
48239601812 <jesusmonteirokarinede@gmail.com>
2026-09-11 14:25:15 -0300

    Exercicio 11

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,18 +2,23 @@ package org.example;
 import java.util.Scanner;
 
 // Exercicios de estrutura de decisão
-// Questão 10 -Aprovação do aluno
+// Exercício 11 - Classificação de Clientes por Investimento
+
 public class Main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        double nota;
-        System.out.println("Digite sua nota:");
-        nota = entrada.nextDouble();
 
-        if (nota >=7 ){
-            System.out.println("Aprovado!!");
+        System.out.println("Informe o valor investido pelo cliente (R$):");
+        double investimento = entrada.nextDouble();
+
+        if (investimento <= 10000) {
+            System.out.println("Classificação do cliente: Bronze");
+        } else if (investimento <= 50000) {
+            System.out.println("Classificação do cliente: Prata");
+        } else if (investimento <= 100000) {
+            System.out.println("Classificação do cliente: Ouro");
         } else {
-            System.out.println("Reprovado!");
+            System.out.println("Classificação do cliente: Platinum");
         }
     }
 }
