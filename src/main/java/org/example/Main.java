19c8b47441b39bdc8ee5f3a2328f601a133936b3
48239601812 <jesusmonteirokarinede@gmail.com>
2026-09-11 14:12:56 -0300

    Exercicio 10

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+import java.util.Scanner;
+
+// Exercicios de estrutura de decisão
+// Questão 10 -Aprovação do aluno
+public class Main {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        double nota;
+        System.out.println("Digite sua nota:");
+        nota = entrada.nextDouble();
+
+        if (nota >=7 ){
+            System.out.println("Aprovado!");
+        } else {
+            System.out.println("Reprovado!");
+        }
+    }
+}
